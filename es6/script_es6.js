2d0587f931503d9f9bd205fb4e0bb99d72f547c6
glaszczyk <info@grzegorzlaszczyk.pl>
2016-08-22 08:04:15 +0200

    Podstawowa struktura HTML

---
+++ es6/script_es6.js
@@ -0,0 +1,8 @@
+
+let customer = { name: "Foo" }; 
+let card = { amount: 7, product: "Bar", unitprice: 42 }; 
+let message = `Hello ${customer.name},
+want to buy ${card.amount} ${card.product} for
+a total of ${card.amount * card.unitprice} bucks?`;
+
+alert(message);
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var babel = require("gulp-babel");
+
+gulp.task('sass', function() {
+	return gulp.src('./sass/main.scss')
+	.pipe(sourcemaps.init())
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer())
+	.pipe(rename('style.css'))
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('./css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task("es6", function () {
+  return gulp.src("./es6/script_es6.js")
+    .pipe(babel())
+    .pipe(gulp.dest("./js"));
+});
+
+gulp.task("default", ["sass"], function(){
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+
+	gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch('./index.html', browserSync.reload);
+	gulp.watch('./es6/*.js', ['es6']);
+});
