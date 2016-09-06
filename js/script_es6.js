db7be026142049b2f93f4ae278a2d46f8b4a0720
glaszczyk <info@grzegorzlaszczyk.pl>
2016-09-06 22:59:05 +0200

    Zmienione portfolio, skills i kontakt

--- js/script_es6.js
+++ js/script_es6.js
@@ -1,13 +1,4 @@
 $(document).ready( function () {
 	console.log("Działa");
 
-	// tworzymy referencje do DOM
-	var $portfolio = $('.portfolio'),
-		$modal = $('.portfolio__modal');
-
-	$portfolio.on('click', '.single-project', function () {
-		$this = $(this);
-		$modal.toggleClass('portfolio__modal--hidden');
-		$modal.child().html($this.find('.single-project__caption').text());
-	});
 });
